7dcd4c325a86b8c2ba66a0eadf78b32ff36ebb22
RubenJ01 <rubeneekhof@gmail.com>
2026-03-12 12:26:45 +0100

    feat(implementation): created a minimum working version that satisfies all unit tests

--- src/Slugger.php
+++ src/Slugger.php
@@ -2,6 +2,9 @@
 
 namespace Rjds\PhpSlugify;
 
+use http\Exception\RuntimeException;
+use Transliterator;
+
 class Slugger
 {
     /**
@@ -13,6 +16,28 @@ class Slugger
      */
     public function slugify(string $text, string $divider = '-'): string
     {
-        return "";
+        if ($text === '') {
+            return 'n-a';
+        }
+
+        $text = strtolower($text);
+
+        $transliterator = Transliterator::create('Any-Latin; Latin-ASCII;');
+        if ($transliterator === null) {
+            throw new RuntimeException("Failed to create Transliterator");
+        }
+
+        $text = $transliterator->transliterate($text);
+        if ($text === false) {
+            throw new RuntimeException("Failed to transliterate string");
+        }
+
+        $text = preg_replace('/[^A-Za-z0-9\-\s]/', '', $text);
+        if ($text === null) {
+            throw new RuntimeException("Something unexpected went wrong");
+        }
+
+        $text = trim($text);
+        return str_replace(' ', $divider, $text);
     }
 }
